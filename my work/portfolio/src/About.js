c2b0d0b648b2b70acdff188bdd8df1480d4f38ce
danielaramiani <daniel.abramiani@icloud.com>
2024-04-03 22:13:12 +0400

    com

---
+++ my work/portfolio/src/About.js
@@ -0,0 +1,14 @@
+// components/About.js
+import React from 'react';
+import './About.css'; // Import CSS file for styling
+
+const About = () => {
+  return (
+    <div className="about">
+      <h2>About Me</h2>
+      <p>Welcome to my portfolio! I'm passionate about web development...</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ my work/portfolio/src/Projects.js
@@ -0,0 +1,21 @@
+// components/Projects.js
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <div>
+        <h3>Project 1</h3>
+        <p>Description of Project 1...</p>
+      </div>
+      <div>
+        <h3>Project 2</h3>
+        <p>Description of Project 2...</p>
+      </div>
+      {/* Add more project cards */}
+    </div>
+  );
+}
+
+export default Projects;
